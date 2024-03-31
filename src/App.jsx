75694562250da27beb4760fade046d931948a06c
Karol <kpulawski13@gmail.com>
2024-03-31 18:41:05 +0100

    Add routes and RouterProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// Imports routes
+import routes from './routes/routes';
+
+const router = createBrowserRouter(routes);
 
 function App() {
   return (
     <>
       <h1>Hello world</h1>
+      <RouterProvider router={router} />
     </>
   );
 }
