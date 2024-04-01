2bc0e3de6878208b80649844bb852d26cee94225
Karol <kpulawski13@gmail.com>
2024-04-01 19:13:35 +0100

    Test for blog component

---
+++ src/tests/Blog.test.jsx
@@ -0,0 +1,29 @@
+import { describe, it, vi } from 'vitest';
+import Blog from '../components/Blog';
+import { render, screen } from '@testing-library/react';
+import { expect } from 'chai';
+
+let fakeFetchData = {
+  data: [
+    { _id: '1', title: 'Post 1', content: 'Text of post 1' },
+    { _id: '2', title: 'Post 2', content: 'Text of post 2' },
+  ],
+};
+
+vi.mock('../components/hooks/usePosts', () => {
+  return {
+    default: () => fakeFetchData,
+  };
+});
+
+describe('Blog component', () => {
+  it('Renders blog component properly', () => {
+    const { container } = render(<Blog />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('Renders posts correctly', () => {
+    render(<Blog />);
+    expect(screen.getAllByRole('heading')).toHaveLength(2);
+  });
+});
