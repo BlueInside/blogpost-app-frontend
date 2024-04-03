6e60602c0fe4facefe7f64cce3162fc243f9e1be
Karol <kpulawski13@gmail.com>
2024-04-03 13:33:37 +0100

    Implement Comment component that displays comments

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -1,6 +1,9 @@
 import usePosts from './hooks/usePosts';
 import { useParams } from 'react-router-dom';
-
+import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+import { decodeHTMLEntities as decodeHE } from '../utils/decodeHTML';
+import { format } from 'date-fns';
 export default function Post() {
   let { postId } = useParams();
   const {
@@ -20,8 +23,57 @@ export default function Post() {
           <p>Author: {post.author.fullname}</p>
           <p>Published on: {post.formattedTimeStamp}</p>
           <p>Comments: {post.comments.length}</p>
+          {post.comments.map((comment) => (
+            <Comment key={comment} postId={postId} commentId={comment} />
+          ))}
         </div>
       </div>
     </div>
   );
 }
+
+function Comment({ postId, commentId }) {
+  const [comment, setComment] = useState(false);
+
+  useEffect(() => {
+    // Fetch details about comments
+    fetch(`http://localhost:3000/posts/${postId}/comments/${commentId}`, {
+      mode: 'cors',
+    })
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error('Server error');
+        }
+        return response.json();
+      })
+      .then((response) => setComment(response))
+      .catch((err) => {
+        throw new Error('Failed fetching comments', err);
+      });
+  }, [postId, commentId]);
+
+  // Handles orphan comments Id
+  if (!comment) {
+    return (
+      <div>
+        <h1>Anonymous</h1>
+        <p>Post has been deleted.</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <div className="meta-data">
+        <h1>{comment.username}</h1>
+        <span>{format(comment.timeStamp, 'd MMM y')}</span>
+      </div>
+      <p>{decodeHE(comment.text)}</p>
+    </div>
+  );
+}
+
+Comment.propTypes = {
+  postId: PropTypes.string,
+  commentId: PropTypes.string,
+};
