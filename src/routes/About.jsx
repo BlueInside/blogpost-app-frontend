19889b943cc0eba3d4727980af2f5c55c051cab0
Karol <kpulawski13@gmail.com>
2024-04-03 16:24:00 +0100

    Add about me page

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,5 +1,72 @@
 function About() {
-  return <h2>About me</h2>;
+  return (
+    <>
+      <article>
+        <h1>My Journey: From Gaming Enthusiast to Web Developer</h1>
+        <p>
+          Hello, everyone! My name is Karol Pulawski, and I'm excited to share
+          my journey with you. I hail from the picturesque town of Sandomierz in
+          Poland, and at 26 years old, I find myself on an exhilarating path
+          into the world of web development.
+        </p>
+        <p>
+          My adventure began about a year and a half ago when I made a pivotal
+          decision to explore a new career path. After completing high school, I
+          found myself at a crossroads, unsure of what direction to take.
+          Seeking clarity, I embarked on a gap year abroad.
+        </p>
+        <p>
+          During my time abroad, I fell into a routine of gaming and work, but
+          deep down, I knew I craved something more fulfilling. It wasn't long
+          before I realized that I needed a change. So, I took a leap of faith
+          and left my job as a Restaurant Manager, determined to reshape my
+          life.
+        </p>
+        <p>
+          Over the past few years, I've undergone a remarkable transformation. I
+          made significant lifestyle changes, including quitting smoking and
+          prioritizing my health with regular gym sessions. But perhaps my most
+          significant endeavor has been my journey into the world of coding.
+        </p>
+        <p>
+          My fascination with coding initially led me to explore game
+          development. However, I soon realized the complexities involved,
+          especially without prior IT knowledge. Undeterred, I shifted my focus
+          to web development after discovering the wealth of resources available
+          online.
+        </p>
+        <p>
+          Fuelled by determination, I dove headfirst into online courses,
+          starting with Colt Steele's acclaimed Udemy course. Through his
+          guidance, I delved into the intricacies of JavaScript and beyond.
+          Currently, I'm immersed in the final stages of the Project Odin
+          curriculum, eager to refine my skills further.
+        </p>
+        <p>
+          Through this blog, I aim to document my experiences and insights as I
+          navigate the exciting world of web development. Moreover, I want to
+          address a common concern among aspiring programmers: the fear of
+          starting late. My journey is proof that it's never too late to pursue
+          your dreams, regardless of age or background.
+        </p>
+        <p>
+          As I continue on this exhilarating journey, I hope to inspire others
+          to follow their passions and embrace change. With each challenge and
+          triumph, I grow more confident in my abilities and more determined to
+          carve out a fulfilling career in web development.
+        </p>
+        <p>
+          Thank you for joining me on this adventure. Let's code our way to
+          success together!
+        </p>
+        <p>
+          Warm regards,
+          <br />
+          Karol Pulawski
+        </p>
+      </article>
+    </>
+  );
 }
 
 export default About;
