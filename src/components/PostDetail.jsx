fd96fc087143b454aa4c63a373b01edb21f640d3
Karol <kpulawski13@gmail.com>
2024-04-03 11:15:51 +0100

    Create PostDetail component

---
+++ src/components/PostDetail.jsx
@@ -0,0 +1,27 @@
+import usePosts from './hooks/usePosts';
+import { useParams } from 'react-router-dom';
+
+export default function Post() {
+  let { postId } = useParams();
+  const {
+    data: post,
+    error,
+    loading,
+  } = usePosts(`http://localhost:3000/posts/${postId}`);
+
+  if (error) return <p>Error</p>;
+  if (loading) return <p>Loading...</p>;
+  return (
+    <div>
+      <div>
+        <h2>{post.title}</h2>
+        <p>{post.content}</p>
+        <div>
+          <p>Author: {post.author.fullname}</p>
+          <p>Published on: {post.formattedTimeStamp}</p>
+          <p>Comments: {post.comments.length}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
