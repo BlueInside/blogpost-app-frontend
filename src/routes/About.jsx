f1e6746eb54c91b7c05712ccccc1ce55896b479d
Karol <kpulawski13@gmail.com>
2024-04-03 16:26:47 +0100

    Remove apostrophes from text

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -4,10 +4,10 @@ function About() {
       <article>
         <h1>My Journey: From Gaming Enthusiast to Web Developer</h1>
         <p>
-          Hello, everyone! My name is Karol Pulawski, and I'm excited to share
-          my journey with you. I hail from the picturesque town of Sandomierz in
-          Poland, and at 26 years old, I find myself on an exhilarating path
-          into the world of web development.
+          Hello, everyone! My name is Karol Pulawski, and I&apos;m excited to
+          share my journey with you. I hail from the picturesque town of
+          Sandomierz in Poland, and at 26 years old, I find myself on an
+          exhilarating path into the world of web development.
         </p>
         <p>
           My adventure began about a year and a half ago when I made a pivotal
@@ -17,16 +17,17 @@ function About() {
         </p>
         <p>
           During my time abroad, I fell into a routine of gaming and work, but
-          deep down, I knew I craved something more fulfilling. It wasn't long
-          before I realized that I needed a change. So, I took a leap of faith
-          and left my job as a Restaurant Manager, determined to reshape my
-          life.
+          deep down, I knew I craved something more fulfilling. It wasn&apos;t
+          long before I realized that I needed a change. So, I took a leap of
+          faith and left my job as a Restaurant Manager, determined to reshape
+          my life.
         </p>
         <p>
-          Over the past few years, I've undergone a remarkable transformation. I
-          made significant lifestyle changes, including quitting smoking and
-          prioritizing my health with regular gym sessions. But perhaps my most
-          significant endeavor has been my journey into the world of coding.
+          Over the past few years, I&apos;ve undergone a remarkable
+          transformation. I made significant lifestyle changes, including
+          quitting smoking and prioritizing my health with regular gym sessions.
+          But perhaps my most significant endeavor has been my journey into the
+          world of coding.
         </p>
         <p>
           My fascination with coding initially led me to explore game
@@ -37,17 +38,17 @@ function About() {
         </p>
         <p>
           Fuelled by determination, I dove headfirst into online courses,
-          starting with Colt Steele's acclaimed Udemy course. Through his
+          starting with Colt Steele&apos;s acclaimed Udemy course. Through his
           guidance, I delved into the intricacies of JavaScript and beyond.
-          Currently, I'm immersed in the final stages of the Project Odin
+          Currently, I&apos;m immersed in the final stages of the Project Odin
           curriculum, eager to refine my skills further.
         </p>
         <p>
           Through this blog, I aim to document my experiences and insights as I
           navigate the exciting world of web development. Moreover, I want to
           address a common concern among aspiring programmers: the fear of
-          starting late. My journey is proof that it's never too late to pursue
-          your dreams, regardless of age or background.
+          starting late. My journey is proof that it&apos;s never too late to
+          pursue your dreams, regardless of age or background.
         </p>
         <p>
           As I continue on this exhilarating journey, I hope to inspire others
@@ -56,7 +57,7 @@ function About() {
           carve out a fulfilling career in web development.
         </p>
         <p>
-          Thank you for joining me on this adventure. Let's code our way to
+          Thank you for joining me on this adventure. Let&apos;s code our way to
           success together!
         </p>
         <p>
