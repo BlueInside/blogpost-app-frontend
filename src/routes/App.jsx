4ac92229341bc2a7ae7c60d0d3dd2f41f03ddeb3
Karol <kpulawski13@gmail.com>
2024-04-09 11:37:05 +0100

    Add welcome text on path /

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,15 +1,27 @@
 import Navbar from '../components/Navbar';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import theme from '../components/StyledComponents/Theme.styled';
 import GlobalStyles from '../components/StyledComponents/Global.Style';
+import styled from 'styled-components';
+const WelcomeMessage = styled.h1`
+  margin-top: 20px;
+  font-size: 1.5rem;
+  text-align: center;
+  color: ${(props) => props.theme.textColor};
+`;
 function App() {
+  let location = useLocation();
+  let isIndexPage = location.pathname === '/';
   return (
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyles />
         <Navbar />
         <main>
+          {isIndexPage && (
+            <WelcomeMessage>Welcome to My BlogPost</WelcomeMessage>
+          )}
           <Outlet />
         </main>
       </ThemeProvider>
