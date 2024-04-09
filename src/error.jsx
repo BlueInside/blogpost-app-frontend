cc0778996dd089fd90bb26f69b980c3ee95c6f0d
Karol <kpulawski13@gmail.com>
2024-04-09 11:36:55 +0100

    Style ErrorPage component

--- src/error.jsx
+++ src/error.jsx
@@ -1,15 +1,41 @@
 import { useRouteError } from 'react-router';
+import styled from 'styled-components';
+import Navbar from './components/Navbar';
+
+const ErrorPageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  font-family: Arial, sans-serif;
+  color: #ffffff;
+  background-color: #333333;
+`;
+
+const ErrorPageHeading = styled.h1`
+  font-size: 3rem;
+  margin-bottom: 20px;
+`;
+
+const ErrorMessage = styled.p`
+  font-size: 1.2rem;
+  margin-bottom: 10px;
+`;
+
 export default function ErrorPage() {
   const error = useRouteError();
-  console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
+    <>
+      <Navbar />
+      <ErrorPageContainer>
+        <ErrorPageHeading>Something Went Wrong</ErrorPageHeading>
+        <ErrorMessage>Sorry, an unexpected error has occurred.</ErrorMessage>
+        <ErrorMessage>
+          <i>{error?.statusText || error?.message}</i>
+        </ErrorMessage>
+      </ErrorPageContainer>
+    </>
   );
 }
